edf20d30bc3b6c5df3f3913f3dca1d6abf781e65
Christian <cegomes.21@est.ucab.edu.ve>
2024-11-10 13:43:50 -0400

    fix: :bug: Importation Fix

--- src/common/application/application-services/application-service.interface.ts
+++ src/common/application/application-services/application-service.interface.ts
@@ -1,4 +1,4 @@
-import { Result } from '../../Domain/result-handler/Result';
+import { Result } from 'src/common/domain/result-handler/result';
 
 export interface IApplicationService<D, R> {
   execute(data: D): Promise<Result<R>>;
