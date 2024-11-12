8f08663d348c0d24048531291ff816579d46b3a2
Christian <cegomes.21@est.ucab.edu.ve>
2024-11-11 21:15:14 -0400

    refactor: :recycle: Remove FirebaseAuthService and related interface from the project

--- src/auth/infrastructure/controller/auth.controller.ts
+++ src/auth/infrastructure/controller/auth.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Post, Body, Inject, Get, Param, ValidationPipe, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { ApiTags } from '@nestjs/swagger';
-import { User } from 'src/auth/application/model/user-model';
 import { UserRepository } from '../repository/user.repository';
-import { FireBaseAuthService } from '../../../common/infrastructure/providers/services/firebase-Auth.service';
 
 @ApiTags('Products')
 @Controller('product')
@@ -13,7 +11,6 @@ export class ProductController {
   constructor(
     @Inject('BaseDeDatos')
     private readonly dataSource: DataSource,
-    private readonly fireBaseService: FireBaseAuthService,
   ) {
     this.userRepository = new UserRepository(this.dataSource);
   }
--- src/common/application/firebase-Auth-service/firebase.aut.service.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface IFirebaseAuthService {
-  saverUserFirebase(email: string, password: string): Promise<string>;
-  loginUserFirebase(email: string, password: string): Promise<string>;
-}
