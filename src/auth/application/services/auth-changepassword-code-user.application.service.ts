5b25dc1d49a88874d60564bd59a21bbbe559f211
Christian <cegomes.21@est.ucab.edu.ve>
2024-11-19 09:43:01 -0400

    .

--- src/auth/application/services/auth-changepassword-code-user.application.service.ts
+++ src/auth/application/services/auth-changepassword-code-user.application.service.ts
@@ -21,6 +21,8 @@ export class ChangePasswordCodeUserApplicationService implements IApplicationSer
       return Result.fail<IForgotPasswordResponseApplication>(null, 404, 'User not found');
     }
 
+    console.log(user.getValue().expirationCodeDate);
+
     const expirationDate = this.dateService.toUtcMinus4(user.getValue().expirationCodeDate);
 
     console.log(expirationDate, this.dateService.now());
