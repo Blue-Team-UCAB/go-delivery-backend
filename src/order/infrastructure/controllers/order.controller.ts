00b413d7cbb2825e2d416464c9caaa418342014e
Christian <cegomes.21@est.ucab.edu.ve>
2025-01-15 01:46:59 -0400

    fix: :bug: Corregir la lógica de verificación del estado del pedido y mejorar el manejo de pasos de ruta en CourierMovement

--- src/order/infrastructure/controllers/order.controller.ts
+++ src/order/infrastructure/controllers/order.controller.ts
@@ -177,7 +177,7 @@ export class OrderController {
       throw new NotFoundException('Order not found');
     }
 
-    /* const cancelledOrDelivered = order.Value.ordenState.some(state => state === OrderStates.CANCELLED || state === OrderStates.DELIVERED);
+    const cancelledOrDelivered = order.Value.ordenState.some(state => state === OrderStates.CANCELLED || state === OrderStates.DELIVERED);
     if (cancelledOrDelivered) {
       throw new NotFoundException('Order is cancelled or delivered');
     }
@@ -185,8 +185,8 @@ export class OrderController {
     const shipped = order.Value.ordenState.some(state => state === OrderStates.SHIPPED);
     if (!shipped) {
       throw new NotFoundException('Order is not shipped');
-    }*/
-    console.log('order', order.Value);
+    }
+
     const courierMovement = new CourierMovement(this.movementRepository);
     return courierMovement.getCourierMovement(order.Value.latitude.toString(), order.Value.longitude.toString(), id);
   }
