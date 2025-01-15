b12d0465962da9082e49cc10579f95ae988c61c7
Christian <cegomes.21@est.ucab.edu.ve>
2025-01-15 14:30:47 -0400

    feat: :sparkles: Agregar autenticación y validación en el endpoint de solicitud de IA

--- src/IA/infrastructure/controllers/controller.ts
+++ src/IA/infrastructure/controllers/controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { ChatGptService } from 'src/common/infrastructure/providers/services/chatGtp.service';
 import { IaMakeRequestDto } from '../dto/ia-make-request.dto';
 import { IaResponseDto } from 'src/common/application/IA-Service/Ia-response.dto';
+import { IsClientOrAdmin } from 'src/auth/infrastructure/jwt/decorator/isClientOrAdmin.decorator';
 
 @ApiTags('IA')
 @Controller('ia')
@@ -13,6 +14,9 @@ export class IAController {
     this.ia_Service = new ChatGptService();
   }
   @Post('make/request')
+  @ApiBody({ type: IaMakeRequestDto })
+  @ApiBearerAuth()
+  @IsClientOrAdmin()
   async makeRequest(@Body() message: IaMakeRequestDto) {
     try {
       const response = await this.ia_Service.makeRequest(message.mensage);
