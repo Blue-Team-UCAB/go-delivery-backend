2ea1c33030d0d8263443e344738aae59216b3edd
Christian <cegomes.21@est.ucab.edu.ve>
2025-01-17 05:05:17 -0400

    fix: :bug: Traducir estados de orden a español en el servicio de cambio de estado de orden

--- src/order/infrastructure/event-listener/change-order-status.service.ts
+++ src/order/infrastructure/event-listener/change-order-status.service.ts
@@ -11,6 +11,25 @@ export class OrderStatusChangeCosumerService<T extends DataDto> implements IList
   @EventPattern('OrderStatusChangeEvent')
   async handle(@Payload() data: T, @Ctx() context: RmqContext) {
     data.data.linkedDivices.forEach(token => {
+      if (data.data.state._state === 'DELIVERED') {
+        data.data.state._state = 'ENTREGADA';
+      }
+      if (data.data.state._state === 'SHIPPED') {
+        data.data.state._state = 'ENVIADA';
+      }
+
+      if (data.data.state._state === 'CANCELLED') {
+        data.data.state._state = 'CANCELADA';
+      }
+
+      if (data.data.state._state === 'IN_PROCESS') {
+        data.data.state._state = 'EN PROCESO';
+      }
+
+      if (data.data.state._state === 'CREATED') {
+        data.data.state._state = 'CREADA';
+      }
+
       const msg: PushNotificationDto = {
         token: token,
         title: `Orden #${data.data.idOrder._id.slice(-5)} actualizada`,
